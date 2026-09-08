25b91443d0a3956b98a9fdddd6664e17da1e3de1
FAHIM HASSAN <faahim180@gmail.com>
2026-09-08 08:21:03 +0600

    Added admin navigation

---
+++ view/adminNav.php
@@ -0,0 +1,10 @@
+<nav class="subnav">
+<a href="admin_dashboard.php">Overview</a>
+<a href="admin_manage_users.php">Users</a>
+<a href="admin_manage_providers.php">Providers</a>
+<a href="admin_manage_schedules.php">Schedules</a>
+<a href="admin_view_bookings.php">Bookings</a>
+<a href="admin_complaints.php">Complaints & feedback</a>
+<a href="admin_payments.php">Payments & refunds</a>
+<a href="operations_dashboard.php">Live overview</a>
+</nav>
