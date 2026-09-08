373fe1f44537e5aba386dbffad7a6a1b2111af2f
FAHIM HASSAN <faahim180@gmail.com>
2026-09-08 08:22:31 +0600

    fixed footer

---
+++ view/footer.php
@@ -0,0 +1,8 @@
+</main>
+<footer>
+<a class="brand" href="index.php">e-ticket.</a>
+<span>Enhanced E-Ticket Reservation & Integrated Travel Management System</span>
+<a href="provider_register.php">Become a service provider</a>
+</footer>
+</body>
+</html>
