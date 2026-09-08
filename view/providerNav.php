150efdf1ab1a26dd8c5f65dbf23697886700676c
FAHIM HASSAN <faahim180@gmail.com>
2026-09-08 08:25:20 +0600

    Added provider navigation menu

---
+++ view/providerNav.php
@@ -0,0 +1,8 @@
+<nav class="subnav">
+<a href="provider_dashboard.php">My services</a>
+<a href="provider_add_schedule.php">Add schedule</a>
+<a href="provider_add_hotel.php">Add hotel</a>
+<a href="provider_bookings.php">Bookings</a>
+<a href="provider_feedback.php">Feedback & complaints</a>
+<a href="operations_dashboard.php">Live overview</a>
+</nav>
